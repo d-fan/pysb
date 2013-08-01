a08c0d9065e8d47b6ec3321363b3d628755daff0
Shawn Garbett <shawn@garbett.org>
2013-08-01 10:45:43 -0500

    Still confused on testing varsens

--- pysb/tools/varsens.py
+++ pysb/tools/varsens.py
@@ -2,26 +2,25 @@
 import pysb.bng
 import ghalton
 import numpy
+import sys
 
-def varsens(model, solver, objective, n, scaling, log_scaling=False, verbose=True):
-
-    pysb.bng.generate_equations(model)
-    k = len(model.odes)
-
+def varsens(model, solver, objective, k, n, scaling, log_scaling=False, verbose=True):
     if verbose: print "Generating Halton sequences"
     seq = ghalton.Halton(k) # half for A, and half for B
-    seq.get(2*(k*k-k)) # Burn away any face exmnmn mn       nm ploration
-    ldA = scale(numpy.array(seq.get(n)), scaling, log_scaling)
-    ldB = scale(numpy.array(seq.get(n)), scaling, log_scaling)
-    ldC = getCmtx(ldA, ldB)
+    seq.get(2*(k*k-k)) # Burn away any face exploration off the Halton
+    M_1  = scale(numpy.array(seq.get(n)), scaling, log_scaling)  # See Eq (9)
+    M_2  = scale(numpy.array(seq.get(n)), scaling, log_scaling)  # See Eq (9)
+    N_j  = generate_N_j(M_1, M_2)                                # See Eq (11)
+    N_nj = generate_N_j(M_2, M_1)
     
-    (yA, yB, yC) = parmeval(model, ldA, ldB, ldC, objective, solver, verbose)
+    (fM_1, fM_2, fN_j, fN_nj) = objective_values(model, M_1, M_2, N_j, N_nj, objective, solver, verbose) 
     
     if verbose: print "Final sensitivity calculation"
-    return getvarsens(yA, yB, yC)
+    return getvarsens(fM_1, fM_2, fN_j, fN_nj)
 
 def scale(points, scaling, log_scaling):
     if log_scaling:
+# FIXME, I THINK THIS IS ALL BACKWARD, Ugh.
         s = numpy.exp(scaling)
         return numpy.log(points*(s[1]-s[0]) + s[0])
     else:
@@ -29,109 +28,138 @@ def scale(points, scaling, log_scaling):
 
 def move_spinner(i):
     spin = ("|", "/","-", "\\")
-    print "\r[%s] %d"%(spin[i%4],i),
+    print "[%s] %d\r"%(spin[i%4],i),
     sys.stdout.flush()
 
-def genCmtx(ldmtxA, ldmtxB):
+def generate_N_j(M_1, M_2):
     """when passing the quasi-random low discrepancy-treated A and B matrixes, this function
     iterates over all the possibilities and returns the C matrix for simulations.
     See e.g. Saltelli, Ratto, Andres, Campolongo, Cariboni, Gatelli, Saisana,
     Tarantola Global Sensitivity Analysis"""
 
-    nparams = ldmtxA.shape[1] # shape 1 should be the number of params
+    nparams = M_1.shape[1] # shape 1 should be the number of params
 
     # allocate the space for the C matrix
-    ldmtxC = numpy.array([ldmtxB]*nparams) 
+    N_j = numpy.array([M_2]*nparams) 
 
-    # Now we have nparams copies of ldmtxB. replace the i_th column of ldmtxC with the i_th column of ldmtxA
+    # Now we have nparams copies of M_2. replace the i_th column of N_j with the i_th column of M_1
     for i in range(nparams):
-        ldmtxC[i,:,i] = ldmtxA[:,i]
+        N_j[i,:,i] = M_1[:,i]
 
-    return ldmtxC
+    return N_j
 
-def parmeval(model, ldmtxA, ldmtxB, ldmtxC, objective, solver, verbose=True): #, fileobj=None):
-    ''' Function parmeval calculates the yA, yB, and yC_i arrays needed for variance-based
+def objective_values(model, M_1, M_2, N_j, N_nj, objective, solver, verbose=True): #, fileobj=None):
+    ''' Function parmeval calculates the fM_1, fM_2, and fN_j_i arrays needed for variance-based
     global sensitivity analysis as prescribed by Saltelli and derived from the work by Sobol
     (low-discrepancy sequences)
     '''
 
-    # assign the arrays that will hold yA, yB and yC_n
-    yA = numpy.zeros([ldmtxA.shape[0]] + [len(model.observable_patterns)])
-    yB = numpy.zeros([ldmtxB.shape[0]] + [len(model.observable_patterns)])
-    yC = numpy.zeros(list(ldmtxC.shape[:2]) + [len(model.observable_patterns)]) # matrix is of shape (nparam, nsamples)
+    # assign the arrays that will hold fM_1, fM_2 and fN_j_n
+    fM_1  = numpy.zeros(M_1.shape[0])
+    fM_2  = numpy.zeros(M_2.shape[0])
+    fN_j  = numpy.zeros([M_1.shape[1]] + [M_1.shape[0]]) # matrix is of shape (nparam, nsamples)
+    fN_nj = numpy.zeros([M_1.shape[1]] + [M_1.shape[0]])
 
     # First process the A and B matrices
-    if verbose: print "processing matrix A:"
-    for i in range(ldmtxA.shape[0]):
-        outlist = solver(model, ldmtxA[i])
-        yA[i]   = objective(outlist)
+    if verbose: print "Processing f(M_1):"
+    for i in range(M_1.shape[0]):
+        fM_1[i]   = objective(solver(model, M_1[i]))
         if verbose: move_spinner(i)
 
-    if verbose: print "processing matrix B:"
-    for i in range(ldmtxB.shape[0]):
-        outlist = solver(model, ldmtxB[i])
-        yB[i]   = objective(outlist)
+    if verbose: print "Processing f(M_2):"
+    for i in range(M_2.shape[0]):
+        fM_2[i]   = objective(solver(model, M_2[i]))
         if verbose: move_spinner(i)
 
-    if verbose: print "processing matrix C_n"
-    for i in range(ldmtxC.shape[0]):
-        if verbose: print "processing processing parameter %d"%i
-        for j in range(ldmtxC.shape[1]):
-            outlist = solver(model, ldmtxC[i][j])
-            yC[i][j] = objective(outlist)
+    if verbose: print "Processing f(N_j)"
+    for i in range(N_j.shape[0]):
+        if verbose: print " * parameter %d"%i
+        for j in range(N_j.shape[1]):
+            fN_j[i][j] = objective(solver(model, N_j[i][j]))
             if verbose: move_spinner(j)
 
-    return yA, yB, yC
+    if verbose: print "Processing f(N_nj)"
+    for i in range(N_j.shape[0]):
+        if verbose: print " * parameter %d"%i
+        for j in range(N_j.shape[1]):
+            fN_nj[i][j] = objective(solver(model, N_nj[i][j]))
+            if verbose: move_spinner(j)
 
-def getvarsens(yA, yB, yC):
-    """Calculate the array of S_i and ST_i for each parameter given yA, yB, yC matrices
+    return fM_1, fM_2, fN_j, fN_nj
+
+def getvarsens(fM_1, fM_2, fN_j, fN_nj):
+    """Calculate the array of S_i and ST_i for each parameter given fM_1, fM_2, fN_j matrices
     from the multi-sampling runs. Calculate S_i and ST_i as follows:
 
     Parameter sensitivity:
     ----------------------
             U_j - E^2 
-    S_j = ------------
+    S_j = ------------                   # Eq (10)
                V(y)
 
-    U_j = 1/n \sum yA * yC_j
+    U_j = 1/(n-1) \sum fM_1 * fN_j       # Eq (12)
 
-    E^2 = 1/n \sum yA * 1/n \sum yB
+    E^2 = 1/n \sum fM_1 * fM_2           # Eq (21)
 
     Total effect sensitivity (i.e. non additive part):
     --------------------------------------------------
                   U_-j - E^2
-     ST_j = 1 - -------------
+     ST_j = 1 - -------------            # Eq (27)
                       V(y)
 
-    U_-j = 1/n \sum yB * yC_j
-
-    E^2 = { 1/n \sum yB * yB }^2
+    U_-j = 1/(n-1) \sum fM_2 * fN_j
 
 
-    In both cases, calculate V(y) from yA and yB
+    In both cases, calculate V(y) from fM_1 and fM_2
 
 
     """
-    nparms   = yC.shape[0] # should be the number of parameters
-    nsamples = yC.shape[1] # should be the number of samples from the original matrix
-    nobs     = yC.shape[-1]    # the number of observables (this is linked to BNG usage, generalize?)
 
-    #first get V(y) from yA and yB
+    nparms   = fN_j.shape[0] # should be the number of parameters
+    nsamples = fN_j.shape[1] # should be the number of samples from the original matrix
 
-    varyA = numpy.var(yA, axis=0, ddof=1)
-    varyB = numpy.var(yB, axis=0, ddof=1)
+    E_2 = sum(fM_1*fM_2) / nsamples      # Eq (21)
 
-    # now get the E^2 values for the S and ST calculations
-    E_s  = numpy.average((yA * yB), axis=0)
-    E_st = numpy.average(yB, axis=0) ** 2
+    # Estimate 
+    U_j  = numpy.sum(fM_1 * fN_j,  axis=1) / (nsamples - 1)  # Eq (12)
+    U_nj = numpy.sum(fM_1 * fN_nj, axis=1) / (nsamples - 1)  # Eq (unnumbered one after 18)
+
+    #estimate V(y) from fM_1 and fM_2
+
+    varfM_1 = numpy.var(fM_1, axis=0, ddof=1)
+    varfM_2 = numpy.var(fM_2, axis=0, ddof=1)
 
     #allocate the S_i and ST_i arrays
-    Sens = numpy.zeros((nparms,nobs))
-    SensT = numpy.zeros((nparms,nobs))
+    Sens  = numpy.zeros(nparms)
+    SensT = numpy.zeros(nparms)
 
     # now get the U_j and U_-j values and store them 
-    for i in range(nparms):
-        Sens[i]  =        (((yA * yC[i]).sum(axis=0)/(nsamples-1.)) - E_s ) / varyA
-        SensT[i] = 1.0 - ((((yB * yC[i]).sum(axis=0)/(nsamples-1.)) - E_st) / varyB)
+    for j in range(nparms):
+        Sens[j]  =       (U_j[j]  - E_2)/varfM_1
+        SensT[j] = 1.0 - (U_nj[j] - E_2)/varfM_1
 
     return Sens, SensT
+
+
+# Working on a test function here
+
+# This is defined on the range [0..1]
+# Eq (29)
+def g_function(x, a):
+    return numpy.prod([gi_function(i, xi, a) for xi,i in enumerate(x)])
+
+# Eq (30)
+def gi_function(xi, i, a):
+    return (abs(4*xi-2)+a[i]) / (1+a[i])
+
+
+model = [0, 0.5, 3, 9, 99, 99]
+
+# Analytical answer, Eq (34)
+answer = 1.0/(3.0* ((numpy.array(model) + 1.0)**2.0))
+
+def g_solver(model, params): return g_function(params, model)
+
+def g_objective(x): return x
+
+v = varsens(model, g_solver, g_objective, 6, 1024, numpy.array([[0.0]*6, [1.0]*6]))
